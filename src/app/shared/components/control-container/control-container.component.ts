7661f47d3ed3259596be7f8bd58e9a8a982bc151
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-03 00:01:57 +0530

    Basic(Arch): Created Control Component for handling Forms Validataions

---
+++ src/app/shared/components/control-container/control-container.component.ts
@@ -0,0 +1,65 @@
+import { Component, Host, Input, Optional, SkipSelf } from "@angular/core";
+import { AbstractControl, ControlContainer, NG_VALUE_ACCESSOR } from "@angular/forms";
+
+@Component({
+  selector: 'app-control-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './control-container.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ControlContainerComponent,
+      multi: true,
+    },
+  ],
+})
+export class ControlContainerComponent {
+  control: AbstractControl | undefined;
+  @Input()
+  name: string = " ";
+  @Input()
+  label: string;
+  @Input()
+  errorMsg = "An error occurred";
+  @Input()
+  tooltipText: any;
+  @Input()
+  isReadOnly = false;
+  @Input()
+  isLabelOnSameLine = false;
+  required = false;
+
+  constructor( @Optional()
+               @Host()
+               @SkipSelf()
+               private controlContainer: ControlContainer,
+               ) {
+  }
+
+  get status() {
+    if (this.control && this.control.validator) {
+      const validator = this.control.validator({} as AbstractControl);
+
+      return validator && validator["required"];
+    }
+  }
+
+  ngOnInit() {
+    if (this.controlContainer) {
+      if (this.name) {
+        this.control = this.controlContainer.control.get(this.name);
+        if (this.control.validator) {
+          const validator = this.control.validator({} as AbstractControl);
+          if (validator && validator["required"]) {
+            this.required = true;
+          }
+        }
+      } else {
+        console.warn("Missing FormControlName directive from host element of the component");
+      }
+    } else {
+      console.warn("Cant find parent FormGroup directive");
+    }
+  }
+}
