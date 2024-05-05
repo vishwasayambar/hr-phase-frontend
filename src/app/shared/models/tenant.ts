a7d3eebe4889cbf50ef60b95fcbe71725a497d60
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-06 00:18:45 +0530

    Basic(Arch): Created Tenant Model

---
+++ src/app/shared/models/tenant.ts
@@ -0,0 +1,23 @@
+
+
+export class Tenant {
+    id: number;
+    name: string;
+    plan: any;
+    whats_app_number: string;
+    support_number: string;
+    support_email: string;
+    sms_credits: number;
+    account_id: number;
+    domain_url: string;
+    logo: string;
+    workspaces: Array<Tenant>;
+    signature: string;
+    trial_ends_at: Date;
+    created_at: Date;
+    updated_at: Date;
+
+    constructor(paramsObject: object) {
+        return Object.assign(this, paramsObject);
+    }
+}
