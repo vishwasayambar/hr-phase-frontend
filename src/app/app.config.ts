2b30136b1759c16be43519f4b63f84a288de19dc
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-08 22:41:04 +0530

    Basic(Arch): Added provider of APP_INITIALIZER to get tenant detail

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from "@angular/common/http";
+import { APP_INITIALIZER, ApplicationConfig } from "@angular/core";
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { AuthInterceptor } from "./shared/interceptor/auth.interceptor";
+import { ConfigService } from "./shared/services/config.service";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+      {
+          provide: APP_INITIALIZER,
+          useFactory: AppConfigFactory,
+          deps: [ConfigService],
+          multi: true,
+      },
+      provideRouter(routes),
+      provideHttpClient(withInterceptors([AuthInterceptor])),
+    ]
 };
+
+export function AppConfigFactory(config: ConfigService) {
+    return () => config.load();
+}
