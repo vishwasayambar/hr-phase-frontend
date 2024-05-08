dfd5b443f2fd7d6d4ddb45ea4e7f42143ddfe32b
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-08 22:39:12 +0530

    feat(Auth): Added auth route and load Auth module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "auth",
+		loadChildren: () => import("./modules/auth/auth.module").then(m => m.AuthModule),
+		title: "HrPhase Authentication",
+	},
+];
