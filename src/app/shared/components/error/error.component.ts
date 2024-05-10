258f910e67720621ea8a2439db681c7ebd0fe108
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-10 23:34:16 +0530

    Removed style declaretion in error component removed scss file

--- src/app/shared/components/error/error.component.ts
+++ src/app/shared/components/error/error.component.ts
@@ -5,7 +5,6 @@ import { Component, input } from "@angular/core";
 	standalone: true,
 	imports: [],
 	templateUrl: "./error.component.html",
-	styleUrl: "./error.component.scss"
 })
 export class ErrorComponent {
 
