a382f368739d331b0fb75ad61fe2864dec500358
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-10 23:28:53 +0530

    Feat(Auth): Update Register path in Constant

--- src/app/shared/constants/constant.ts
+++ src/app/shared/constants/constant.ts
@@ -5,7 +5,7 @@ const hostOrigin = window.location.origin;
 let subDomain = hostOrigin.substring(hostOrigin.indexOf("/") + 2, hostOrigin.indexOf("."));
 subDomain = subDomain !== "" ? subDomain + "." : "";
 export const SERVER_URL = `${window.location.protocol}//${environment.baseUrl + API_PREFIX}`;
-export const REGISTRATION_PATH = "register";
+export const REGISTRATION_PATH = "registerTenant";
 export const AUTH_PATH = "auth/";
 
 export enum TENANT_PLANS {
