44d9af67428a4b402ae1f06c049b28ac47c94a2d
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-10 23:31:47 +0530

    Basic(Arch): Added Common Form Validation in Base component

--- src/app/shared/base-component.ts
+++ src/app/shared/base-component.ts
@@ -1,8 +1,9 @@
 import { Injector } from "@angular/core";
-import { UntypedFormBuilder } from "@angular/forms";
+import { UntypedFormArray, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 
 export abstract class BaseComponent {
+	isMobileDevice = window.innerWidth < 760;
 	router: Router;
 	activatedRouter: ActivatedRoute;
 	fb: UntypedFormBuilder;
@@ -10,5 +11,26 @@ export abstract class BaseComponent {
 		this.router = injector.get(Router);
 		this.activatedRouter = injector.get(ActivatedRoute);
 		this.fb = injector.get(UntypedFormBuilder);
+	}
+
+	validateFormFields(form: UntypedFormGroup | UntypedFormArray): void {
+		Object.keys(form.controls).forEach(field => {
+			const control = form.get(field);
+			if (control instanceof UntypedFormControl) {
+				control.markAsTouched();
+				control.updateValueAndValidity(); // Reruns the custom validators if any
+			} else if (control instanceof UntypedFormGroup) {
+				this.validateFormFields(control);
+			} else if (control instanceof UntypedFormArray) {
+				Object.keys(control.controls).forEach(index => {
+					if (control instanceof UntypedFormControl) {
+						control.markAsTouched();
+						control.updateValueAndValidity();
+					} else {
+						this.validateFormFields(control.get(index) as UntypedFormGroup);
+					}
+				});
+			}
+		});
 	}
 }
