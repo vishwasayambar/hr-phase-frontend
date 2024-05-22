b51c5cce386fc0a15bf73532a5a76115def37112
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-22 23:47:53 +0530

    pint fixes

--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from "@angular/core";
-// import { ENTITIES } from "../constants/constants";
-// import { Customer } from "../models/customer";
-// import { Employee } from "../models/employee";
+import { Employee } from "../models/employee";
 import { Tenant } from "../models/tenant";
 
 @Injectable({providedIn: "root"})
 export class StorageService {
-	// setUser(user: Customer | Employee): void {
-	// 	localStorage.setItem("currentUser", JSON.stringify(user));
-	// }
+	setUser(user: Employee): void {
+		localStorage.setItem("currentUser", JSON.stringify(user));
+	}
 
 	getUser(): any {
 		return JSON.parse(localStorage.getItem("currentUser"));
@@ -56,9 +54,6 @@ export class StorageService {
 		if (token) {
 			const payload = this.payload(token);
 			if (payload) {
-				// return Object.values(this.iss).indexOf(payload.iss) > -1
-				//     ? true
-				//     : false;
 				return true;
 			}
 		}
@@ -92,11 +87,4 @@ export class StorageService {
 		localStorage.removeItem("permissions");
 	}
 
-	// clearCache(): void {
-	// 	Object.values(ENTITIES).forEach(entity => {
-	// 		if (entity?.name) {
-	// 			localStorage.removeItem(entity?.name);
-	// 		}
-	// 	});
-	// }
 }
