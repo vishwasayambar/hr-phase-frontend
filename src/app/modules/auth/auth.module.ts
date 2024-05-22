962c9cc9320b567808a9f7a32f861ddc5bf81c82
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-22 23:44:43 +0530

    Feat(Auth): Created AccountActivateComponent for generating password

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { ControlContainerComponent } from "../../shared/components/control-container/control-container.component";
+import { ErrorComponent } from "../../shared/components/error/error.component";
+import { AccountActivateComponent } from "./account-activate/account-activate.component";
 import { AppLogoComponent } from "./app-logo/app-logo.component";
 import { AuthRoutingModule } from "./auth-routing.module";
 import { AuthComponent } from "./auth.component";
@@ -14,13 +16,15 @@ import { SignupComponent } from "./signup/signup.component";
 		AuthComponent,
 		LoginComponent,
 		SignupComponent,
-		AppLogoComponent
+		AppLogoComponent,
+		AccountActivateComponent,
 	],
 	imports: [
 		CommonModule,
 		AuthRoutingModule,
 		ControlContainerComponent,
 		ReactiveFormsModule,
+		ErrorComponent
 	]
 })
 export class AuthModule {
