3d462d9b4f29ff3b5404ced431013b279843c359
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-05-30 20:36:45 +0530

    Feat: Created Save btn common component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from "@angular/common";
 import { NgModule, Type } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { SaveBtnComponent } from './components/save-btn/save-btn.component';
 
 const COMMON_COMP_LIST: any[] | Type<any> = [
-
+	SaveBtnComponent
 ];
 
 @NgModule({
@@ -12,11 +13,11 @@ const COMMON_COMP_LIST: any[] | Type<any> = [
         COMMON_COMP_LIST,
 
     ],
-    exports: [
-        COMMON_COMP_LIST,
-        FormsModule,
-        ReactiveFormsModule,
-    ],
+	exports: [
+		COMMON_COMP_LIST,
+		FormsModule,
+		ReactiveFormsModule,
+	],
     imports: [
         CommonModule,
         FormsModule,
