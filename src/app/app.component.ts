fa14a2726ff4bc6567b668266e9cb70fcfe496c7
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-06-03 19:35:46 +0530

    Feat(Arch): Added flowbite library for UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
+import { NgClass } from "@angular/common";
 import { Component, Injector } from "@angular/core";
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from "flowbite";
 import { BaseComponent } from "./shared/base-component";
+import { NavbarComponent } from "./shared/layouts/navbar/navbar.component";
+import { SidebarComponent } from "./shared/layouts/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass, SidebarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -13,6 +17,7 @@ export class AppComponent extends BaseComponent  {
   title = 'hr-pahse-frontend';
   constructor(injector: Injector) {
     super(injector);
+    initFlowbite();
   }
 
   ngOnInit(): void {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ],
 }
 
