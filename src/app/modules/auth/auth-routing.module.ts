c22a37681986a7f115611dcba587a16110f8e2f9
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-06-06 19:02:43 +0530

    Refactor Auth routing module

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 import { AuthGuard } from "../../shared/guards/auth.guard";
 import { AccountActivateComponent } from "./account-activate/account-activate.component";
 import { AuthComponent } from "./auth.component";
@@ -7,39 +7,40 @@ import { LoginComponent } from "./login/login.component";
 import { SignupComponent } from "./signup/signup.component";
 
 const routes: Routes = [
-  {
-    path: "",
-    component: AuthComponent,
-    children: [
-      {
-        path: "login",
-        component: LoginComponent,
-        canActivate: [AuthGuard],
-        title: "HrPhase Login",
-      },
-      {
-        path: "signup",
-        component: SignupComponent,
-        // canActivate: [BeforeLoginGuard],
-        title: "HrPhase SignUp",
-      },
-      {
-        path: "accountActivate/:activation_code",
-        component: AccountActivateComponent,
-        // canActivate: [BeforeLoginGuard],
-        title: "HrPhase Account Activation",
-      },
-      {
-        path: "",
-        redirectTo: "/auth/login",
-        pathMatch: "full",
-      },
-  ]
-  },
+	{
+		path: "",
+		component: AuthComponent,
+		children: [
+			{
+				path: "login",
+				component: LoginComponent,
+				canActivate: [AuthGuard],
+				title: "HrPhase Login",
+			},
+			{
+				path: "signup",
+				component: SignupComponent,
+				// canActivate: [BeforeLoginGuard],
+				title: "HrPhase SignUp",
+			},
+			{
+				path: "accountActivate/:activation_code",
+				component: AccountActivateComponent,
+				// canActivate: [BeforeLoginGuard],
+				title: "HrPhase Account Activation",
+			},
+			{
+				path: "",
+				redirectTo: "/auth/login",
+				pathMatch: "full",
+			},
+		]
+	},
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {
+}
