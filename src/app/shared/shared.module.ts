6e726a15c4b9912f049825607d9169bc9ddaea4f
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-06-06 19:02:10 +0530

    Feat(ARCH): Import standalone comp in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,18 @@ import { NgModule, Type } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
 import { SaveBtnComponent } from './components/save-btn/save-btn.component';
+import { HoverDirective } from "./directives/hover.directive";
+import { UserPopoverComponent } from "./popovers/user-popover/user-popover.component";
+import { DropdownActionComponent } from "./ui-components/dropdown-action/dropdown-action.component";
 
 const COMMON_COMP_LIST: any[] | Type<any> = [
 	SaveBtnComponent
+];
+
+const COMMON_STANDALONE_COMP_LIST: any[] | Type<any> = [
+	DropdownActionComponent,
+	HoverDirective,
+	UserPopoverComponent,
 ];
 
 @NgModule({
@@ -15,10 +24,12 @@ const COMMON_COMP_LIST: any[] | Type<any> = [
     ],
 	exports: [
 		COMMON_COMP_LIST,
+		COMMON_STANDALONE_COMP_LIST,
 		FormsModule,
 		ReactiveFormsModule,
 	],
     imports: [
+		COMMON_STANDALONE_COMP_LIST,
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
