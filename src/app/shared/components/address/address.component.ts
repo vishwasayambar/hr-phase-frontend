101a0281053f9c34f45420ec8c52ccf84fe8b990
Vishwa Sayambar <vishwajitsayambar92@gmail.com>
2024-06-13 22:44:54 +0530

    Feat: Created Address and bank Component

---
+++ src/app/shared/components/address/address.component.ts
@@ -0,0 +1,44 @@
+import { Component, Injector, Input, OnInit } from "@angular/core";
+import { FormArray, FormsModule, ReactiveFormsModule, UntypedFormArray, UntypedFormGroup } from "@angular/forms";
+import { BaseComponent } from "../../base-component";
+import { Address } from "../../models/address";
+import { ControlContainerComponent } from "../control-container/control-container.component";
+
+@Component({
+	selector: "app-address",
+	standalone: true,
+	imports: [
+		ControlContainerComponent,
+		FormsModule,
+		ReactiveFormsModule
+	],
+	templateUrl: "./address.component.html",
+})
+export class AddressComponent extends BaseComponent implements OnInit {
+	@Input() form: UntypedFormGroup;
+	@Input() id = "address";
+	stateList: any = [];
+	cityList: any = [];
+
+	constructor(injector: Injector) {
+		super(injector);
+	}
+
+	ngOnInit(): void {
+
+	}
+
+	get addressControls(): FormArray {
+		return this.form.get("address") as FormArray;
+	}
+
+	removeAddress(i: number): void {
+		const customFields = this.form.get("address") as UntypedFormArray;
+		customFields.removeAt(i);
+	}
+
+	addAddress(): void {
+		const fields = this.form.get("address") as UntypedFormArray;
+		fields.push(Address.getForm(new Address({})));
+	}
+}
