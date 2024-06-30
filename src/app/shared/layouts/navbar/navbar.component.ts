24c0035df39d9473cd77e60dcdb85971d2400b4d
Vishwajit Sayambar <vishwajitsayambar92@gmail.com>
2024-06-30 19:14:28 +0530

    Feat(Account Settings): Write route for account settings in navbar profile dropdown

--- src/app/shared/layouts/navbar/navbar.component.ts
+++ src/app/shared/layouts/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component, Injector } from "@angular/core";
 import { BaseComponent } from "../../base-component";
 import { ToggleThemeComponent } from "../toggle-theme/toggle-theme.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
 	selector: "app-navbar",
 	standalone: true,
-	imports: [
-		ToggleThemeComponent
-	],
+    imports: [
+        ToggleThemeComponent,
+        RouterLink
+    ],
 	templateUrl: "./navbar.component.html",
 	styleUrl: "./navbar.component.scss"
 })
