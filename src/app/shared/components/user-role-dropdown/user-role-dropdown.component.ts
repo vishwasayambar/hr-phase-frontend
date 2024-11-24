205f24d5a88c68e0cd5bc33255a5b5ebb5ba1e73
Vishwa <vishwajitsayambar92@gmail.com>
2024-11-24 17:35:04 +0530

    Impr: Used select Dropdown in role component

--- src/app/shared/components/user-role-dropdown/user-role-dropdown.component.ts
+++ src/app/shared/components/user-role-dropdown/user-role-dropdown.component.ts
@@ -1,15 +1,11 @@
-import { Component, EventEmitter, forwardRef, Injector, Input, OnInit, Output } from "@angular/core";
-import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
-import { BaseComponent } from "../../base-component";
-import { RoleService } from "../../services/role.service";
+import {Component, EventEmitter, forwardRef, Injector, Input, OnInit, Output} from "@angular/core";
+import {ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule} from "@angular/forms";
+import {BaseComponent} from "../../base-component";
+import {RoleService} from "../../services/role.service";
+import {Role} from "../../models/role";
 
 @Component({
 	selector: "app-user-role-dropdown",
-	standalone: true,
-	imports: [
-		ReactiveFormsModule,
-		FormsModule
-	],
 	templateUrl: "./user-role-dropdown.component.html",
 	providers: [
 		{
@@ -26,53 +22,57 @@ export class UserRoleDropdownComponent extends BaseComponent implements OnInit, 
 	@Input() id = "role";
 	@Output() itemClick: EventEmitter<any> = new EventEmitter();
 	roles: any = [];
-
+	showDropdown = false;
+	roleName: string;
+	
 	constructor(injector: Injector, private service: RoleService) {
 		super(injector);
 	}
-
+	
 	// eslint-disable-next-line @angular-eslint/no-input-rename
 	@Input("value") _value: any = null;
-
+	
 	get value() {
 		return this._value;
 	}
-
+	
 	set value(val) {
 		this._value = val;
 		this.onChange(val);
 		this.onTouched();
 	}
-
+	
 	ngOnInit(): void {
 		this.service.getRoles().subscribe({
 			next: (res) => {
 				this.roles = res;
 				this.value = res[0].id;
+				this.roleName = res[0].display_name
 			},
 		});
 	}
-
+	
 	onChange: any = () => {
 	};
-
+	
 	onTouched: any = () => {
 	};
-
+	
 	registerOnChange(fn: any) {
 		this.onChange = fn;
 	}
-
+	
 	writeValue(value: any) {
 		this.value = value;
 	}
-
+	
 	registerOnTouched(fn: any) {
 		this.onTouched = fn;
 	}
-
-	onValueChange(event: Event) {
-		this.value = (event.target as HTMLSelectElement).value;
+	
+	onValueChange(event: Role) {
+		this.value = event.id;
+		this.roleName = event.display_name
 		this.itemClick.emit(this.value);
 	}
 }
