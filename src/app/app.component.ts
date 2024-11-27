764f163acf991965467227534a2f27a1aec18bf0
Vishwa <vishwajitsayambar92@gmail.com>
2024-11-27 09:50:55 +0530

    Config(FlowBite): Updated Flowbite config that model is noty working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {NgClass} from "@angular/common";
-import {Component, Injector} from "@angular/core";
-import {RouterOutlet} from '@angular/router';
+import { Component, Injector, OnInit } from "@angular/core";
+import { NavigationEnd, RouterOutlet } from '@angular/router';
 import {initFlowbite} from "flowbite";
 import {BaseComponent} from "./shared/base-component";
 import {NavbarComponent} from "./shared/layouts/navbar/navbar.component";
@@ -13,15 +13,21 @@ import {SidebarComponent} from "./shared/layouts/sidebar/sidebar.component";
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
-export class AppComponent extends BaseComponent {
+export class AppComponent extends BaseComponent implements OnInit {
     title = 'hr-phase-frontend';
 
     constructor(injector: Injector) {
         super(injector);
-        initFlowbite();
+        // TODO:: This will work direct when we add "initFlowbite();" but Modal is not working on router-outlet components therefor we need below code.
+        this.router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                setTimeout(() => {  initFlowbite();})
+            }
+        });
     }
 
     ngOnInit(): void {
+        initFlowbite();
         this.loadingService.isLoading$.subscribe((isLoading) => {
             this.isLoading = isLoading;
         });
