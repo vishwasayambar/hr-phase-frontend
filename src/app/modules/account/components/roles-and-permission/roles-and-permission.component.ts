80e04a796cde93891c28c052451489a5be3ed2f5
Vishwa <vishwajitsayambar92@gmail.com>
2024-11-27 09:56:56 +0530

    Feat(Role): HP-5 Updated Roles and Permission mapping component

--- src/app/modules/account/components/roles-and-permission/roles-and-permission.component.ts
+++ src/app/modules/account/components/roles-and-permission/roles-and-permission.component.ts
@@ -1,117 +1,136 @@
-import {Component, Injector, OnInit} from '@angular/core';
-import {MODULE_WISE_PERMISSION_TABS} from "../../../../shared/constants/constant";
-import {BaseComponent} from "../../../../shared/base-component";
-import {RoleService} from "../../../../shared/services/role.service";
-import {PermissionService} from "../../../../shared/services/permission.service";
-import {Role} from "../../../../shared/models/role";
-import {forkJoin} from "rxjs";
+import { Component, Injector, OnInit } from '@angular/core';
+import { BaseComponent } from "../../../../shared/base-component";
+import { RoleService } from "../../../../shared/services/role.service";
+import { PermissionService } from "../../../../shared/services/permission.service";
+import { Role } from "../../../../shared/models/role";
+import { forkJoin } from "rxjs";
+import { Permission } from "../../../../shared/models/permission";
 
 @Component({
-    selector: 'app-roles-and-permission',
-    templateUrl: './roles-and-permission.component.html',
+	selector: 'app-roles-and-permission',
+	templateUrl: './roles-and-permission.component.html',
 })
 export class RolesAndPermissionComponent extends BaseComponent implements OnInit {
-    moduleWisePermissionTabs = Object.values(MODULE_WISE_PERMISSION_TABS);
-    currentTab: string;
-    selectedModuleName: string;
-    selectedModulePermission: any[] = [];
-    roles: Role[] = [];
-    roleWisePermissionArray: any[] = [];
-    allPermissions: any[] = [];
-    groupedPermissions: any[] = [];
-    selectedPermissions: any[] = [];
-    value: number;
-    isSavingPermissions = false;
-    permissionId: number;
-    modules: any = [
-
-    ]
-
-    constructor(injector: Injector, private service: RoleService, private permissionService: PermissionService) {
-        super(injector);
-    }
-
-    ngOnInit() {
-        forkJoin({
-            roles: this.service.getRoles(),
-            permissions: this.permissionService.getAll(),
-        }).subscribe({
-            next: (res) => {
-                this.roles = res.roles;
-                this.value = res.roles[0]?.id; // Use optional chaining to avoid errors if roles array is empty
-                this.allPermissions = res.permissions;
-                if (this.value) {
-                    this.onValueChange(this.value);
-                }
-            },
-            error: (err) => {
-                console.error('Error loading roles or permissions', err);
-            }
-        });
-    }
-
-    savePermission(){
-        if(this.value){
-        this.isSavingPermissions = true
-            this.permissionService.saveRolePermissions(this.value, this.selectedPermissions).subscribe({
-                next: (res) => {
-                    console.log(res)
-                },
-                error: (err) => {
-                    console.log(err)
-                },
-            }).add(() => {
-                this.isSavingPermissions = false
-            })
-        }
-    }
-
-    onValueChange(value: number) {
-        this.value = value;
-        console.log(value);
-
-            this.permissionService.getByRoleId(value).subscribe({
-                next: res => {
-                    this.roleWisePermissionArray = res;
-                        this.groupPermissions();
-                    console.log(this.roleWisePermissionArray, "Permission Array");
-                },
-                error: err => {
-                    console.log(err)
-                }
-            })
-    }
-
-    groupPermissions() {
-        this.groupedPermissions = [];
-        for (let key in this.roleWisePermissionArray) {
-            if (this.roleWisePermissionArray.hasOwnProperty(key)) {
-                this.groupedPermissions.push({
-                    moduleName: key,
-                    permissions: this.roleWisePermissionArray[key]
-                });
-                this.selectedModulePermission = this.roleWisePermissionArray[this.groupedPermissions[0]['moduleName']];
-            }
-        }
-    }
-
-    setModulePermissions(moduleName:any){
-        this.selectedModuleName = moduleName;
-        this.selectedModulePermission = this.roleWisePermissionArray[moduleName]
-        console.log(this.selectedModulePermission);
-    }
-
-    onPermissionChange(permission: any, event: any) {
-        debugger
-        if (event.target.checked) {
-            this.selectedPermissions.push(permission);
-        } else {
-            const index = this.selectedPermissions.findIndex(p => p.id === permission.id);
-            if (index > -1) {
-                this.selectedPermissions.splice(index, 1);
-            }
-        }
-    }
-
-
+	selectedModuleName: string;
+	selectedModulePermission: any[] = [];
+	roles: Role[] = [];
+	roleWisePermissionArray: any[] = [];
+	allPermissions: Permission[] = [];
+	selectedPermissions: any[] = [];
+	deSelectedPermissions: any[] = [];
+	currentRoleId: number;
+	currentRole: Role;
+	isSavingPermissions = false;
+	filteredPermissionOfRole: { moduleName: string, permissions: any[] }[];
+	
+	constructor(injector: Injector, private service: RoleService, private permissionService: PermissionService) {
+		super(injector);
+	}
+	
+	ngOnInit() {
+		// Load roles and permissions in parallel using forkJoin
+		forkJoin({
+			roles: this.service.getRoles(),
+			permissions: this.permissionService.getAll(),
+		}).subscribe({
+			next: (res) => {
+				this.roles = res.roles; // Save roles
+				this.currentRoleId = res.roles[0]?.id; // Default to the first role Id
+				this.currentRole = res.roles[0]; // Default to the first role
+				this.allPermissions = res.permissions; // Save permissions
+				if (this.currentRoleId) {
+					this.onRoleChange(this.currentRole); // Load permissions for the selected role
+				}
+			},
+			error: (err) => {
+				console.error('Error loading roles or permissions', err);
+			},
+		});
+	}
+	
+	// Save the selected and deselected permissions for the current role
+	syncRolePermission() {
+		if (this.currentRoleId) {
+			this.isSavingPermissions = true
+			this.permissionService.saveRolePermissions(this.currentRoleId, {
+				selectedPermission: this.selectedPermissions,
+				deSelectedPermission: this.deSelectedPermissions
+			}).subscribe({
+				next: (res) => {
+					this.onRoleChange(this.currentRole); // To fetch the updated permissions
+				},
+				error: (err) => {
+					console.log(err)
+				},
+			}).add(() => {
+				this.isSavingPermissions = false
+			})
+		}
+	}
+	
+	
+	mapPermissionsWithRoles(allPermissions: any[], roleWisePermissions: any[]) {
+		return allPermissions.map(module => {
+			const moduleName = module.name;
+			
+			// Get permissions for the current module from the role-wise array
+			const moduleRolePermissions = roleWisePermissions[moduleName] || [];
+			
+			return {
+				moduleName: moduleName,
+				permissions: module.items.map((permission: { name: any; display_name: any; }) => {
+					// Check if the permission is included in the moduleRolePermissions
+					const hasPermission = moduleRolePermissions.some((rolePermission: {
+						name: any;
+					}) => rolePermission.name === permission.name);
+					
+					return {
+						name: permission.name,
+						display_name: permission.display_name,
+						hasPermission: hasPermission,
+					};
+				}),
+			};
+		});
+	}
+	
+	// When the role changes, update the permissions view like get roles selected and deselected permissions
+	onRoleChange(role: Role) {
+		this.currentRoleId = role.id;
+		this.permissionService.getByRoleId(role.id).subscribe({
+			next: res => {
+				this.roleWisePermissionArray = res;
+				this.filteredPermissionOfRole = this.mapPermissionsWithRoles(this.allPermissions, this.roleWisePermissionArray)
+				this.selectedModulePermission = this.filteredPermissionOfRole[0]?.permissions;
+				this.selectedModuleName = this.filteredPermissionOfRole[0]?.moduleName;
+			},
+			error: err => {
+				console.log(err)
+			}
+		})
+	}
+	
+	getModulePermissions(moduleName: any) {
+		this.selectedModuleName = moduleName;
+		this.selectedModulePermission = this.filteredPermissionOfRole.find(res => res.moduleName === moduleName)?.permissions;
+	}
+	
+	onPermissionChange(permission: any, event: any) {
+		if (event.target.checked) {
+			this.selectedPermissions.push(permission);
+		} else {
+			this.deSelectedPermissions.push(permission);
+		}
+	}
+	
+	roleCreated(role: Role) {
+		debugger;
+		this.roles = [role, ...this.roles]; // Combines new Role and creating a new reference
+		this.currentRole = role;
+		this.onRoleChange(role);
+	}
+	
+	filterChange(event: any) {
+		//TODO:: This is for if we have add something on search in Input of Role select box
+	}
 }
