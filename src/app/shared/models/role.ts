a7284c66dc9e113e5d91e387b6af17d5dd93b35c
Vishwa <vishwajitsayambar92@gmail.com>
2024-11-27 19:12:53 +0530

    Feat(Role): HP-5 Updated Role Model for support form

--- src/app/shared/models/role.ts
+++ src/app/shared/models/role.ts
@@ -1,11 +1,23 @@
+import { UntypedFormBuilder, Validators } from "@angular/forms";
+
 export class Role {
     id: number;
     name: string;
     display_name: string;
+    guard_name: string;
     created_at: Date;
     updated_at: Date;
 
     constructor(paramsObject: object) {
         return Object.assign(this, paramsObject);
+    }
+    
+    static getForm(role: Role) {
+        return new UntypedFormBuilder().group({
+            id: [role.id || null],
+            name: [role.name || null, [Validators.required]],
+            display_name: [role.display_name || null, [Validators.required]],
+            guard_name: [role.display_name || 'api', [Validators.required]],
+        });
     }
 }
