312c9b3842d04575be85f4e2a9dbd91f1782d9d3
Vishwa <vishwajitsayambar92@gmail.com>
2024-11-28 21:45:23 +0530

    Impr: UUpdated User Dropdown of navbar from dummy to real

--- src/app/shared/layouts/navbar/navbar.component.ts
+++ src/app/shared/layouts/navbar/navbar.component.ts
@@ -1,21 +1,30 @@
-import { Component, Injector } from "@angular/core";
+import { Component, Injector, OnInit } from "@angular/core";
 import { BaseComponent } from "../../base-component";
 import { ToggleThemeComponent } from "../toggle-theme/toggle-theme.component";
 import {RouterLink} from "@angular/router";
+import { SharedModule } from "../../shared.module";
+import { COMPONENT_SIZES } from "../../constants/constant";
 
 @Component({
 	selector: "app-navbar",
 	standalone: true,
-    imports: [
-        ToggleThemeComponent,
-        RouterLink
-    ],
+	imports: [
+		ToggleThemeComponent,
+		RouterLink,
+		SharedModule
+	],
 	templateUrl: "./navbar.component.html",
 	styleUrl: "./navbar.component.scss"
 })
-export class NavbarComponent extends BaseComponent {
-
+export class NavbarComponent extends BaseComponent implements OnInit {
+	authUser = this.currentUser;
 	constructor(injector: Injector) {
 		super(injector);
 	}
+	
+	ngOnInit(): void {
+	
+	}
+	
+	protected readonly COMPONENT_SIZES = COMPONENT_SIZES;
 }
