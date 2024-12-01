03054fcbb0c72f79027b6e0f3f5b0b8a6de8df61
Vishwa <vishwajitsayambar92@gmail.com>
2024-12-01 17:37:20 +0530

    Feat(Employee): HP-3 Updated Employee Profile page and added routing for employee permissions

--- src/app/modules/employee/components/employee-profile/employee-profile.component.ts
+++ src/app/modules/employee/components/employee-profile/employee-profile.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Injector, OnInit, signal } from '@angular/core';
+import { Employee } from "../../../../shared/models/employee";
+import { EmployeeService } from "../../../../shared/services/employee.service";
+import { BaseComponent } from "../../../../shared/base-component";
+import { filter } from "rxjs";
+import { NavigationEnd } from "@angular/router";
 
 @Component({
-  selector: 'app-employee-profile',
-  templateUrl: './employee-profile.component.html',
-  styleUrl: './employee-profile.component.scss'
+	selector: 'app-employee-profile',
+	templateUrl: './employee-profile.component.html',
 })
-export class EmployeeProfileComponent {
-
+export class EmployeeProfileComponent extends BaseComponent implements OnInit{
+	userId = signal(+this.activatedRouter.snapshot.paramMap.get("employee_id"));
+	currentTabId = signal(+this.activatedRouter.firstChild.snapshot?.data['tab_id']);
+	user: Employee;
+	employeeProfileTabs: any = [
+		{
+			id: 0,
+			name: 'Profile',
+			link: 'profiles',
+		},
+		{
+			id: 1,
+			name: 'Permission',
+			link: 'permissions',
+		}
+	]
+	
+	constructor(injector: Injector, protected service: EmployeeService) {
+		super(injector);
+	}
+	
+	ngOnInit(): void {
+      this.fetchEmployee();
+		this.trackTabChanges();
+	}
+	
+	private trackTabChanges() {
+		// Listen to navigation changes to update the active tab.
+		this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+			const currentPath = this.activatedRouter.firstChild?.snapshot.url[0]?.path || '';
+			const activeTab = this.employeeProfileTabs.find((tab: { link: string; }) => tab.link === currentPath);
+			if (activeTab) {
+				debugger;
+				this.currentTabId.set(activeTab.id);
+			}
+		});
+	}
+	
+	private fetchEmployee() {
+		this.service.getById(this.userId()).subscribe({
+			next: res => {
+				this.user = res;
+			},
+			error: err => {
+				this.showErrorInNotifier(err);
+			},
+		})
+	}
 }
--- src/app/modules/employee/employee-routing.module.ts
+++ src/app/modules/employee/employee-routing.module.ts
@@ -4,6 +4,9 @@ import { EmployeeAddComponent } from "./components/employee-add/employee-add.com
 import { EmployeeListComponent } from "./components/employee-list/employee-list.component";
 import { EmployeeComponent } from "./employee.component";
 import { EmployeeProfileComponent } from "./components/employee-profile/employee-profile.component";
+import {
+	UserRoleAndPermissionComponent
+} from "./components/user-role-and-permission/user-role-and-permission.component";
 
 const routes: Routes = [
 	{
@@ -31,7 +34,18 @@ const routes: Routes = [
 						path: "profiles",
 						component: EmployeeAddComponent,
 						title: "Employee Profile",
-					}
+						data: {
+							tab_id: 0,
+						}
+					},
+					{
+						path: "permissions",
+						component: UserRoleAndPermissionComponent,
+						title: "Employee Profile",
+						data: {
+							tab_id: 1,
+						}
+					},
 				]
 			},
 			{
