b5549688bd986017d21c4b5c998f1674b615965a
Vishwa <vishwajitsayambar92@gmail.com>
2024-12-01 09:17:09 +0530

    Archi: Updated select-dropdown and made it generic using template type

--- src/app/shared/ui-components/select-dropdown/select-dropdown.component.ts
+++ src/app/shared/ui-components/select-dropdown/select-dropdown.component.ts
@@ -1,27 +1,38 @@
-import {Component, input, Input, OnInit, output} from '@angular/core';
+import { Component, input, Input, OnInit, output } from '@angular/core';
+import { ENTITIES } from "../../constants/constant";
 
 @Component({
 	selector: 'app-select-dropdown',
 	templateUrl: './select-dropdown.component.html',
 })
 export class SelectDropdownComponent implements OnInit {
+	protected readonly ENTITIES = ENTITIES;
 	@Input() options: any[] = [];
 	placeholder = input('Search...');
+	templateType = input('');
+	defaultSelection = input();
 	selectionChange = output<any>();
 	value: any;
 	showDropdown = false;
 	
-	// TODO:: Need to Update this to made it generic by using template like some option dont have display_name and id some are direct name it should like searchable dropdown only diff is its not writable
 	ngOnInit() {
-		this.value = this.options[0].display_name;
+		if (this.templateType() === ENTITIES.ROLE) {
+			this.value = this.options[0].display_name;
+		} else {
+			this.value = this.defaultSelection();
+		}
 	}
 	
 	onValueChange(option: any) {
-		this.value = option.display_name
+		if (this.templateType() === ENTITIES.ROLE) {
+			this.value = option.display_name
+		} else {
+			this.value = option
+		}
 		this.selectionChange.emit(option);
 	}
 	
 	hideDropdown() {
-		setTimeout(() => this.showDropdown = false, 200); // Delay to allow item selection
+		setTimeout(() => this.showDropdown = false, 100); // Delay to allow item selection
 	}
 }
