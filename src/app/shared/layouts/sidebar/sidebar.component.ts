330b26334a78b6584ac55b0977a79f4ab2b4b8dc
Vishwa <vishwajitsayambar92@gmail.com>
2025-01-02 09:28:21 +0530

    Impr: Changed Sidebar menu icon

--- src/app/shared/layouts/sidebar/sidebar.component.ts
+++ src/app/shared/layouts/sidebar/sidebar.component.ts
@@ -45,7 +45,7 @@ export class SidebarComponent extends BaseComponent implements OnInit {
             {
                 title: "Dashboard",
                 id: "sidebarDashboard",
-                icon: "M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z",
+                icon: "bi bi-speedometer2",
                 active: false,
                 type: "simple",
                 link: "/dashboards",
@@ -54,7 +54,7 @@ export class SidebarComponent extends BaseComponent implements OnInit {
             {
                 title: "Employees",
                 id: "sidebarEmployees",
-                icon: "M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z",
+                icon: "bi bi-people",
                 active: false,
                 type: "simple",
                 link: "/employees",
